054e1ad3e27b97f7bce808809353d90439351c1e
Renata Cristina <renatacgs14@gmail.com>
2026-03-22 18:35:34 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL -- Senha armazenada com Hash Bcrypt
+    senha VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tarefas (
