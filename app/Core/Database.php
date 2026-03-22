e72700e3e65f692e6e95d4114415aec4402ef627
Renata Cristina <renatacgs14@gmail.com>
2026-03-22 18:35:01 -0300

    Update Database.php

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -9,7 +9,6 @@ class Database {
     public static function getConnection() {
         if (!self::$instance) {
             try {
-                // Configuração padrão do XAMPP: host=localhost, user=root, password vazio
                 self::$instance = new PDO("mysql:host=localhost;dbname=techtrack", "root", "");
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $e) {
